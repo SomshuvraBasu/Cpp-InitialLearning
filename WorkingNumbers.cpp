89e997007c941651447d31c66bad001a7035e7ae
Somshuvra Basu <somshuvra.edu@gmail.com>
2021-12-23 19:06:18 +0530

    Updated Numbers

--- WorkingNumbers.cpp
+++ WorkingNumbers.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
+    int num1 = 3;
+    double num2 = 35.50;
+    num1++;     /*icrements */
+    num1 += 30; /*adds 30 to the number similar can be used for multiply divide etc */
+    cout << pow(2, 3) << fmax(3, 9) << endl;
 
     return 0;
 }
