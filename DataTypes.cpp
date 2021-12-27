990083789edded4bb24096eea770d33a24d95429
Somshuvra Basu <somshuvra.edu@gmail.com>
2021-12-27 08:59:02 +0530

    single line comment (//)

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -5,7 +5,7 @@ int main()
     char grade = 'A';
     string phrase = "UpAcad";
     int marks = 97;
-    float prcnt = 82.45; /* use double intially float would come later on*/
+    float prcnt = 82.45; //use double intially float would come later on
     double cgi = 8.72;
     bool isPass = true;
 
