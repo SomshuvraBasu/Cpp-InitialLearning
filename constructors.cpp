0b9b7a6c7b001542e9316660a0da26bfcc54f954
Somshuvra Basu <somshuvra.edu@gmail.com>
2021-12-28 22:59:45 +0530

    constructor update

--- constructors.cpp
+++ constructors.cpp
@@ -6,6 +6,12 @@ public:
     string title;
     string author;
     int pages;
+    Book()
+    {
+        title = "empty";
+        author = "empty";
+        pages = 0;
+    }
     Book(string aTitle, string aAuthor, int aPages)
     {
         cout << "creating object" << endl;
@@ -17,8 +23,10 @@ public:
 int main()
 {
     Book book1("1971", "Ian Cardozo", 340);
-    Book book2("Trojan","Anurag",540);
+    Book book2("Trojan", "Anurag", 540);
+    Book book3;
     cout << book1.title << endl;
+    cout << book3.title << endl;
 
     return 0;
 }
